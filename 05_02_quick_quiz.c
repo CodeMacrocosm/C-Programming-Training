a31dc0084b7f1622133856037e6301cafd274b99
Shreya Malogi <5hre9a@gmail.com>
2021-04-15 18:02:04 +0530

    cwh

---
+++ 05_02_quick_quiz.c
@@ -0,0 +1,25 @@
+//to print gm, ga gn by using functions
+
+#include<stdio.h>
+void goodMorning();
+void goodAfternoon();
+void goodNight();
+
+int main(){
+    goodMorning();
+    goodAfternoon();
+    goodNight();
+    return 0;
+}
+
+void goodMorning(){
+    printf("Good Morning shreya\n");
+}
+
+void goodAfternoon(){
+    printf("Good Afternoon shreya\n");
+}
+
+void goodNight(){
+    printf("Good Night shreya\n");
+}
